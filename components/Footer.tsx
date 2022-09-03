9b2b1faaf4dcd92950b69a4bfdb23f14968470e1
imaik129 <63268317+imaik129@users.noreply.github.com>
2022-09-04 01:03:04 +0900

    change back size

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => (
   
-  <div className="absolute w-full min-h-fit bg-white">
+  <div className="fixed w-full min-h-fit bg-white">
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
     <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400 mb-10 text-center">© 2022 <a href="https://spirofy.com/" className="hover:underline ">Sporify v1</a>. By imaik All Rights Reserved.
     </span>
